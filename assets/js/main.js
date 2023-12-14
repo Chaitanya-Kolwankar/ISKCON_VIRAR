0cc554c636517ed491389880ada183ebec57ca7b
Chaitanya-Kolwankar <chaitanyakolwankar@gmail.com>
2023-12-14 11:51:20 +0530

    changes to index slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -269,7 +269,7 @@
 
   $(document).ready(function(){
     $('.customer-logos').slick({
-        slidesToShow: 3,
+        slidesToShow: 4,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2500,
@@ -277,14 +277,14 @@
         dots: false,
         pauseOnHover: false,
         responsive: [{
-            breakpoint: 768,
+            breakpoint: 1140,
             settings: {
-                slidesToShow: 4
+                slidesToShow: 3
             }
         }, {
             breakpoint: 520,
             settings: {
-                slidesToShow: 3
+                slidesToShow: 1
             }
         }]
     });
